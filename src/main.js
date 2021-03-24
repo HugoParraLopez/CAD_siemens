4c3aa047a6dca822c96775f19a995e5aded91c8b
DiegoRz <adamantwolf@hotmail.com>
2021-03-24 00:22:10 -0600

    Changed Bootstrap to vue-bootstrap

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VuePdf from 'vue-pdf'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 import { routes } from './js/routes.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(VueRouter)
 Vue.use(VuePdf)
+
 
 const router = new VueRouter({
     routes,
