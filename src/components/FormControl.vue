52f2f091f0d0d5cae7cac1d8e325fbb6b6ff86c2
DiegoRz <adamantwolf@hotmail.com>
2021-05-21 21:02:09 -0500

    Created control panel component to generate JSON file of ocntents

---
+++ src/components/FormControl.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="formContainer">
+            <p v-for="(item, index) in getJSON()" :key="index">
+                {{ item }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { files } from './../js/files'
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getJSON() {
+            return files
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.formContainer {
+    padding: 20% 5%;
+    margin: auto;
+    align-content: center;
+}
+@media only screen and (max-width: 600px) {
+    .formContainer {
+        padding: 35% 5%
+    }
+}
+
+</style>
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,16 @@
 import Rest from './../components/Rest'
 import Main from './../components/Main'
+import FormControl from './../components/FormControl'
 
 export const routes = [{
         path: '/info',
         name: 'main',
         component: Main,
+    },
+    {
+        path: '/quickpanel',
+        name: 'cp',
+        component: FormControl,
     },
     { path: '/', component: Rest, name: 'rest' },
     { path: '*', redirect: '/' }
